ca1a455a3812276019d68c1c68949bbb51e697d0
Klint Tiley <kctiley@gmail.com>
2015-11-05 13:44:55 -0700

    Completed up to and including 01-5 Password Strength

---
+++ main.js
@@ -0,0 +1,10 @@
+// var app = angular.module('app', []);
+
+// app.controller('PasswordController', function($scope) {
+//     $scope.password = ;
+//     $scope.isEven = true;
+//     $scope.increment = function() {
+//       $scope.val += 1;
+//       $scope.isEven = $scope.val % 2 === 0;
+//     };
+// });
